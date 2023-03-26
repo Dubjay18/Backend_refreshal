3f86a8a5c55daf3f2062f6676d6fa378066e93ed
Dubjay18 <jejeniyi7@gmail.com>
2023-03-26 19:17:34 +0100

    feat:Note app update

--- Notes_app/app.js
+++ Notes_app/app.js
@@ -7,9 +7,45 @@ yargs.version("1.1.0");
 yargs.command({
   command: "add",
   describe: "Add a new note",
+  builder: {
+    title: {
+      describe: "Note title",
+      demandOption: true,
+      type: "string",
+    },
+    body: {
+      describe: "Note body",
+      demandOption: true,
+      type: "string",
+    },
+  },
+  handler: function (argv) {
+    console.log("Title" + argv.title, "Body" + argv.body);
+  },
+});
+// remove command
+yargs.command({
+  command: "remove",
+  describe: "Remove a note",
   handler: function () {
-    console.log("Adding a new note!");
+    console.log(chalk.red.underline("Removing the note!"));
+  },
+});
+// list command
+yargs.command({
+  command: "list",
+  describe: "list a note",
+  handler: function () {
+    console.log(chalk.bgBlue.white("List of notes!"));
+  },
+});
+// read command
+yargs.command({
+  command: "read",
+  describe: "read a note",
+  handler: function () {
+    console.log(chalk.blue("Reading the note!"));
   },
 });
 
-console.log(yargs.argv);
+yargs.parse();
