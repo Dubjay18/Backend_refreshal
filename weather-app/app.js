56eaffaa87f8cf0f6ee09a30eb2a5753cf33be60
Dubjay18 <jejeniyi7@gmail.com>
2023-07-22 11:17:06 +0100

    feat: weather app module 5

--- weather-app/app.js
+++ weather-app/app.js
@@ -4,6 +4,12 @@ const url =
   "http://api.weatherstack.com/current?access_key=c3046b32be6d569c31c0d94402eb64b1&query=37.8267,-122.4233";
 
 request({ url, json: true }, (error, response) => {
-  const data = response.body;
-  console.log(data.current);
+  console.log(
+    response.body.current.weather_descriptions[0] +
+      ". It is currently " +
+      response.body.current.temperature +
+      " degress out. It feels like " +
+      response.body.current.feelslike +
+      " degrees out."
+  );
 });
