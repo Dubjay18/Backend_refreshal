5b8f0d8034186ea868de3922ae62f6d893acf30d
Dubjay18 <jejeniyi7@gmail.com>
2023-07-24 23:19:16 +0100

    user update

--- task-manager/src/index.js
+++ task-manager/src/index.js
@@ -41,6 +41,32 @@ app.get("/users/:id", (req, res) => {
       res.status(500).send(e);
     });
 });
+app.patch("/users/:id", async (req, res) => {
+  const updates = Object.keys(req.body);
+  const allowedUpdates = ["name", "email"];
+  const isValidOperation = updates.every((update) =>
+    allowedUpdates.includes(update)
+  );
+  if (!isValidOperation) {
+    res.status(400).send({ error: "Invalid updates!" });
+  }
+  try {
+    const user = await User.findByIdAndUpdate(
+      req.params.id,
+      req.body,
+      {
+        new: true,
+        runValidators: true,
+      }
+    );
+    if (!user) {
+      return res.status(404).send();
+    }
+    res.send(user);
+  } catch (e) {
+    return res.status(400).send(e);
+  }
+});
 
 app.post("/tasks", (req, res) => {
   const task = new Task(req.body);
