6bea79817d424bcd77465cd85ec4fe7b638e2a3f
Dubjay18 <jejeniyi7@gmail.com>
2023-07-24 23:02:57 +0100

    module resource creation 1

--- task-manager/src/db/mongoose.js
+++ task-manager/src/db/mongoose.js
@@ -1,5 +1,6 @@
 const mongoose = require("mongoose");
-const validator = require("validator");
+const User = require("../models/users");
+const Task = require("../models/tasks");
 
 mongoose
   .connect("mongodb://127.0.0.1:27017/taskmanager", {
@@ -9,82 +10,31 @@ mongoose
   .then(() => console.log("Database connected!"))
   .catch((err) => console.log(err));
 console.log("Connected correctly");
-const User = mongoose.model("User", {
-  name: {
-    type: String,
-    required: true,
-    trrim: true,
-  },
-  email: {
-    type: String,
-    required: true,
-    trim: true,
-    lowercase: true,
-    validate(value) {
-      if (!validator.isEmail(value)) {
-        throw new Error("Email  is invalid");
-      }
-    },
-  },
-  password: {
-    type: String,
-    required: true,
-    minlength: 7,
-    trim: true,
-    validate(value) {
-      if (value.toLowerCase().includes("password")) {
-        throw new Error(
-          "Password cannot contained 'password' "
-        );
-      }
-    },
-  },
-  age: {
-    type: Number,
-    validate(value) {
-      if (value < 0) {
-        throw new Error("Age must be a positive number");
-      }
-    },
-  },
-});
 
-const me = new User({
-  name: "Mike",
-  email: "Myemail@Head.to",
-  password: "qwertyuiop",
-});
+// const me = new User({
+//   name: "Mike",
+//   email: "Myemail@Head.to",
+//   password: "qwertyuiop",
+// });
 
-me.save()
-  .then(() => {
-    console.log(me);
-  })
-  .catch((error) => {
-    console.log("Error!", error);
-  });
+// me.save()
+//   .then(() => {
+//     console.log(me);
+//   })
+//   .catch((error) => {
+//     console.log("Error!", error);
+//   });
 
-const Task = mongoose.model("Task", {
-  description: {
-    type: String,
-    trim: true,
-    required: true,
-  },
-  completed: {
-    type: Boolean,
-    default: false,
-  },
-});
+// const task = new Task({
+//   description: "Learn the Mongoose library",
+//   completed: false,
+// });
 
-const task = new Task({
-  description: "Learn the Mongoose library",
-  completed: false,
-});
-
-task
-  .save()
-  .then(() => {
-    console.log(task);
-  })
-  .catch((error) => {
-    console.log("Error!", error);
-  });
+// task
+//   .save()
+//   .then(() => {
+//     console.log(task);
+//   })
+//   .catch((error) => {
+//     console.log("Error!", error);
+//   });
--- task-manager/src/index.js
+++ task-manager/src/index.js
@@ -1,12 +1,21 @@
 const express = require("express");
-
+const User = require("./models/users");
+require("./db/mongoose");
 const app = express();
 const port = process.env.PORT || 3000;
 
 app.use(express.json());
 
 app.post("/users", (req, res) => {
-  res.send("testing!");
+  const user = new User(req.body);
+  user
+    .save()
+    .then(() => {
+      res.send(user);
+    })
+    .catch((e) => {
+      res.status(400).send(e);
+    });
 });
 app.listen(port, () => {
   console.log("Server is up on port " + port);
