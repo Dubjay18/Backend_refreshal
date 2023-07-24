b3b18204d574a99ffe12850e7d93cb164c86b22c
Dubjay18 <jejeniyi7@gmail.com>
2023-07-24 21:08:14 +0100

    validation and sanitization

--- task-manager/src/db/mongoose.js
+++ task-manager/src/db/mongoose.js
@@ -1,4 +1,5 @@
 const mongoose = require("mongoose");
+const validator = require("validator");
 
 mongoose
   .connect("mongodb://127.0.0.1:27017/taskmanager", {
@@ -8,34 +9,69 @@ mongoose
   .then(() => console.log("Database connected!"))
   .catch((err) => console.log(err));
 console.log("Connected correctly");
-// const User = mongoose.model("User", {
-//   name: {
-//     type: String,
-//   },
-//   age: {
-//     type: Number,
-//   },
-// });
+const User = mongoose.model("User", {
+  name: {
+    type: String,
+    required: true,
+    trrim: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    trim: true,
+    lowercase: true,
+    validate(value) {
+      if (!validator.isEmail(value)) {
+        throw new Error("Email  is invalid");
+      }
+    },
+  },
+  password: {
+    type: String,
+    required: true,
+    minlength: 7,
+    trim: true,
+    validate(value) {
+      if (value.toLowerCase().includes("password")) {
+        throw new Error(
+          "Password cannot contained 'password' "
+        );
+      }
+    },
+  },
+  age: {
+    type: Number,
+    validate(value) {
+      if (value < 0) {
+        throw new Error("Age must be a positive number");
+      }
+    },
+  },
+});
 
-// const me = new User({
-//   name: "Andrew",
-//   age: 27,
-// });
+const me = new User({
+  name: "Mike",
+  email: "Myemail@Head.to",
+  password: "qwertyuiop",
+});
 
-// me.save()
-//   .then(() => {
-//     console.log(me);
-//   })
-//   .catch((error) => {
-//     console.log("Error!", error);
-//   });
+me.save()
+  .then(() => {
+    console.log(me);
+  })
+  .catch((error) => {
+    console.log("Error!", error);
+  });
 
 const Task = mongoose.model("Task", {
   description: {
     type: String,
+    trim: true,
+    required: true,
   },
   completed: {
     type: Boolean,
+    default: false,
   },
 });
 
