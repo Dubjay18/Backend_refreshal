9620d6c0dc004bbfaa6ae82533fe0e84702dc1e6
Dubjay18 <jejeniyi7@gmail.com>
2023-07-24 23:04:48 +0100

    Task creation

--- task-manager/src/index.js
+++ task-manager/src/index.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const User = require("./models/users");
+const Task = require("./models/tasks");
 require("./db/mongoose");
 const app = express();
 const port = process.env.PORT || 3000;
@@ -17,6 +18,18 @@ app.post("/users", (req, res) => {
       res.status(400).send(e);
     });
 });
+app.post("/tasks", (req, res) => {
+  const task = new Task(req.body);
+  task
+    .save()
+    .then(() => {
+      res.send(task);
+    })
+    .catch((e) => {
+      res.status(400).send(e);
+    });
+});
+
 app.listen(port, () => {
   console.log("Server is up on port " + port);
 });
