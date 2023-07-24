bdd49140a6cdc57b1866f1bb9dd492a73e9ae8cb
Dubjay18 <jejeniyi7@gmail.com>
2023-07-24 23:14:40 +0100

    tasks get requests

--- task-manager/src/index.js
+++ task-manager/src/index.js
@@ -54,6 +54,30 @@ app.post("/tasks", (req, res) => {
     });
 });
 
+app.get("/tasks", (req, res) => {
+  Task.find({})
+    .then((tasks) => {
+      res.status(200).send(tasks);
+    })
+    .catch((e) => {
+      res.status(500).send(e);
+    });
+});
+
+app.get("/tasks/:id", (req, res) => {
+  const _id = req.params.id;
+  Task.findById(_id)
+    .then((task) => {
+      if (!task) {
+        res.status(404).send({ message: "Task not found" });
+      }
+      res.status(200).send(task);
+    })
+    .catch((e) => {
+      res.status(500).send(e);
+    });
+});
+
 app.listen(port, () => {
   console.log("Server is up on port " + port);
 });
