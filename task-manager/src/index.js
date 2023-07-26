295a667a561c93f38752b1ace853c7f6631e1142
Dubjay18 <jejeniyi7@gmail.com>
2023-07-26 13:00:22 +0100

    feat added task update

--- task-manager/src/index.js
+++ task-manager/src/index.js
@@ -68,6 +68,22 @@ app.patch("/users/:id", async (req, res) => {
   }
 });
 
+app.put("/users/:id", async (req, res) => {
+  try {
+    const user = await User.findByIdAndUpdate(
+      req.params._id,
+      req.body,
+      {
+        new: true,
+        runValidators: true,
+      }
+    );
+    return res.status(200).send(user);
+  } catch (e) {
+    return res.status(400).send(e);
+  }
+});
+
 app.post("/tasks", (req, res) => {
   const task = new Task(req.body);
   task
@@ -102,6 +118,35 @@ app.get("/tasks/:id", (req, res) => {
     .catch((e) => {
       res.status(500).send(e);
     });
+});
+
+app.patch("/tasks/:id", async (req, res) => {
+  const updates = Object.keys(req.body);
+  const allowedUpdates = ["description", "completed"];
+  const isValidOperation = updates.every((update) =>
+    allowedUpdates.includes(update)
+  );
+  if (!isValidOperation) {
+    res.status(400).send({ error: "Invalid updates!" });
+  }
+  try {
+    const task = await Task.findByIdAndUpdate(
+      req.params.id,
+      req.body,
+      {
+        new: true,
+        runValidators: true,
+      }
+    );
+    if (!task) {
+      return res
+        .status(404)
+        .send({ message: "Task not found" });
+    }
+    return res.status(200).send(task);
+  } catch (e) {
+    return res.status(400).send(e);
+  }
 });
 
 app.listen(port, () => {
