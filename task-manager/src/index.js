a2e01531d1bf019b256f3dee160f4551d1a06f53
Dubjay18 <jejeniyi7@gmail.com>
2023-08-08 01:20:23 +0100

    feat:added maintenace middleware

--- task-manager/src/index.js
+++ task-manager/src/index.js
@@ -5,6 +5,14 @@ const taskRouter = require("./routes/task");
 const app = express();
 const port = process.env.PORT || 3000;
 
+app.use((req, res, next) => {
+  res
+    .send({
+      message: "site under maintenance,check back soon",
+    })
+    .status(503);
+});
+
 app.use(express.json());
 app.use(userRouter);
 app.use(taskRouter);
