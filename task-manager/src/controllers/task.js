6a384917a41305dd0f8c8214ce5398fbcac88dea
Dubjay18 <jejeniyi7@gmail.com>
2023-08-11 23:27:29 +0100

    chore:fix task controller

--- task-manager/src/controllers/task.js
+++ task-manager/src/controllers/task.js
@@ -8,20 +8,20 @@ class TaskController {
   }
   async createTask() {
     const task = new Task({
-      ...req.body,
-      owner: req.user._id,
+      ...this.req.body,
+      owner: this.req.user._id,
     });
     try {
       await task.save();
-      res.status(201).send(task);
+      this.res.status(201).send(task);
     } catch (error) {
-      res.status(400).send(error);
+      this.res.status(400).send(error);
     }
   }
   async getTasks() {
     try {
       const tasks = await Task.find({
-        owner: req.user._id,
+        owner: this.req.user._id,
       });
       res.status(200).send(tasks);
     } catch (error) {
@@ -29,11 +29,11 @@ class TaskController {
     }
   }
   async getTaskById() {
-    const _id = req.params.id;
+    const _id = this.req.params.id;
     try {
       const task = await Task.findOne({
         _id,
-        owner: req.user._id,
+        owner: this.req.user._id,
       });
       if (!task) {
         res.status(404).send({ message: "Task not found" });
@@ -45,7 +45,7 @@ class TaskController {
   }
 
   static async updateTask() {
-    const updates = Object.keys(req.body);
+    const updates = Object.keys(this.req.body);
     const allowedUpdates = ["description", "completed"];
     const isValidOperation = updates.every((update) =>
       allowedUpdates.includes(update)
@@ -58,8 +58,8 @@ class TaskController {
 
     try {
       const task = await Task.findOne({
-        _id: req.params.id,
-        owner: req.user._id,
+        _id: this.req.params.id,
+        owner: this.req.user._id,
       });
 
       if (!task) {
@@ -68,7 +68,7 @@ class TaskController {
           .send({ message: "Task not found" });
       }
       updates.forEach((update) => {
-        task[update] = req.body[update];
+        task[update] = this.req.body[update];
       });
       await task.save();
       return res.status(200).send(task);
@@ -80,8 +80,8 @@ class TaskController {
   async deleteTask() {
     try {
       const task = await Task.findOneAndDelete({
-        _id: req.params.id,
-        owner: req.user._id,
+        _id: this.req.params.id,
+        owner: this.req.user._id,
       });
       if (!task) {
         return res
