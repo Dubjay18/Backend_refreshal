2dd0ca0ffda1b2d8a85c3a62587f1371ef6377d3
Dubjay18 <jejeniyi7@gmail.com>
2023-09-04 23:58:31 +0100

    chore

--- chat-app/src/config/server/index.ts
+++ chat-app/src/config/server/index.ts
@@ -17,7 +17,7 @@ export const secureServerListen = (
   const connect = () => {
     server
       .listen(PORT, (): void => {
-        JLogger(`Server is up on port ${PORT}😎`);
+        JLogger(`Server is up on port ${PORT}`);
       })
       .on("error", (err: any) => {
         console.error(err);
